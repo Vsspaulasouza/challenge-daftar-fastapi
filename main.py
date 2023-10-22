28f9649eb59c61bf1330b23045bfcf06f642a798
Vinícius Soares <vsspaulasouza@gmail.com>
2023-10-21 21:24:44 -0300

    feat: changing post stories and add get stories

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import uvicorn
 from fastapi import FastAPI
 
 from src.application.services.stories_api import stories_router
@@ -5,3 +6,7 @@ from src.application.services.stories_api import stories_router
 app = FastAPI()
 
 app.include_router(stories_router)
+
+# FOR DEBUG
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/application/services/stories_api.py
+++ src/application/services/stories_api.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy import exc
 
 from src.interface.models.story import Stories
 from src.interface.models.story_base import StoryBase, db_dependency
@@ -6,9 +7,20 @@ from src.interface.models.story_base import StoryBase, db_dependency
 stories_router = APIRouter()
 
 
-@stories_router.post("/stories/")
+@stories_router.get("/stories/")
+async def list_stories(db: db_dependency):
+    result = db.query(Stories).all()
+    return result
+
+
+@stories_router.post("/stories/", status_code=status.HTTP_201_CREATED)
 async def create_stories(story: StoryBase, db: db_dependency):
     db_story = Stories(title=story.title, text=story.text)
-    db.add(db_story)
-    db.commit()
-    db.refresh(db_story)
+    try:
+        db.add(db_story)
+        db.commit()
+        db.refresh(db_story)
+    except exc.DBAPIError as e:
+        detail = e.orig.diag.message_detail
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+    return db_story
